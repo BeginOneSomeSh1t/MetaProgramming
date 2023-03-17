511182a520a6ba3f57bd4eb2d5aecb4763a4be8b
BeginOneSomeSh1t <Meganite@mail.ru>
2023-03-17 14:20:18 +0400

    working with time literals

--- MetaProgramming/Main.cpp
+++ MetaProgramming/Main.cpp
@@ -1,9 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <chrono>
-#include <ratio>
-#include <cmath>
 #include <iomanip>
-#include <optional>
+#include <chrono>
 
 #define STD_ON using namespace std
 
@@ -13,42 +11,34 @@ void print_whatever(Args...args)
 	((std::cout << args), ...);
 }
 
-using seconds = std::chrono::duration<double>;
-
-using miliseconds = std::chrono::duration<double, std::ratio_multiply<seconds::period, std::milli>>;
-
-using microseconds = std::chrono::duration<double, std::ratio_multiply<seconds::period, std::micro>>;
-
-
-static std::pair<std::string, seconds> get_input()
+std::ostream& operator<<(std::ostream& os, const std::chrono::time_point<std::chrono::system_clock>& t)
 {
-	std::string s;
-	const auto tic{ std::chrono::steady_clock::now() };
-	if (!(std::cin >> s))
-		return { {}, {} };
-	const auto toc{ std::chrono::steady_clock::now() };
-	return { s, toc - tic };
+	const auto tt{ std::chrono::system_clock::to_time_t(t) };
+	const auto loct{ std::localtime(&tt) };
+	return os << std::put_time(loct, "%c");
+}
+
+using days = std::chrono::duration 
+< std::chrono::hours::rep, 
+	std::ratio_multiply<std::chrono::hours::period, std::ratio<24>>>;
+
+
+constexpr days operator ""_days(unsigned long long h)
+{
+	return days{ h };
 }
 
 int main(int argc, char**argv)
 {
 	STD_ON;
-	while (true)
-	{
-		print_whatever("Please, type the word \"C++17\" as fast as you can.\n");
-		const auto [user_input, diff] {get_input()};
-		if (user_input == "") break;
-		if (user_input == "C++17")
-		{
-			print_whatever("Bravo!. You did that!:\n");
-			print_whatever(fixed, setprecision(2), setw(12), diff.count(), " seconds.\n");
-			print_whatever(setw(12), miliseconds(diff).count(), " miliseconds.\n");
-			print_whatever(setw(12), microseconds(diff).count(), " microseconds.\n");
-			break;
-		}
-		else
-			print_whatever("Sorry, your input does not match, try again!\n");
-	}
+	auto now{ chrono::system_clock::now() };
+	print_whatever("The current date and time is ", now, '\n');
 
+	chrono::hours chrono_12h{ 12 };
+	print_whatever("In 12 hourse it'll be: ", (now + chrono_12h), '\n');
+
+	using namespace chrono_literals;
+	print_whatever("12 hours and 15 minutes ago it was ", (now - 12h - 15min), '\n');
+	print_whatever("One week ago: ", (now - 7_days), '\n');
 	
 }
