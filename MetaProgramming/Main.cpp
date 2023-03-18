76bfde130781046fc1af6f352eb3b844d3af0ff5
BeginOneSomeSh1t <Meganite@mail.ru>
2023-03-18 20:11:06 +0400

    go and take some sleep like the threads did ^)

--- MetaProgramming/Main.cpp
+++ MetaProgramming/Main.cpp
@@ -1,9 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <vector>
-#include <random>
-#include <algorithm>
-#include <execution>
+#include <chrono>
+#include <thread>
 
 template<typename...Args>
 void print_whatever(Args...args)
@@ -12,27 +10,19 @@ void print_whatever(Args...args)
 }
 
 using namespace std;
-
-static bool odd(int n) { return n % 2; }
-
+using namespace chrono_literals;
 
 int main(int argc, char**argv)
 {
-	vector<int> d(60000000);
-	mt19937 gen;
-	uniform_int_distribution<int> dis(0, 100000);
-	auto rand_num([=]()mutable {return dis(gen); });
+	cout << "Going to sleep for 5 seconds and 300 ms.\n";
+	this_thread::sleep_for(5s + 300ms);
 
-	generate(execution::par, begin(d), end(d), rand_num);
+	cout << "Sleep for another 3 seconds\n";
+	this_thread::sleep_until(
+		chrono::high_resolution_clock::now() + 3s
+	);
 
-	sort(execution::par, begin(d), end(d));
-
-	reverse(execution::par, begin(d), end(d));
-
-	auto odds(count_if(execution::par, begin(d), end(d), odd));
-
-	cout << (100.0 * odds / d.size())
-		<< " % of the numbers are odd.\n";
+	cout << "That's it";
 	
 
 }
