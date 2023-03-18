0cff594176468c1e97282b01a251f60f8a9a766d
BeginOneSomeSh1t <Meganite@mail.ru>
2023-03-18 18:04:50 +0400

    cool thing about shared_ptr

--- MetaProgramming/Main.cpp
+++ MetaProgramming/Main.cpp
@@ -1,9 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <iomanip>
 #include <memory>
+#include <string>
 
-#define STD_ON using namespace std
 
 template<typename...Args>
 void print_whatever(Args...args)
@@ -11,37 +10,36 @@ void print_whatever(Args...args)
 	((std::cout << args), ...);
 }
 
-struct Foo {
-	int value;
-	Foo(int i) : value{ i } {}
-	~Foo() { std::cout << "DTOR Foo " << value << '\n'; }
+using namespace std;
+
+struct person
+{
+	string name;
+	size_t age;
+	person(string n, size_t a)
+		: name{ move(n) }, age{ a }
+	{
+		print_whatever("Ctor ", name, '\n');
+	}
+	~person() { print_whatever("DCTOR ", name, '\n'); }
 };
 
-void weak_ptr_info(const std::weak_ptr<Foo>& p)
-{
-	print_whatever("-------------", std::boolalpha, "\nexpired: ", p.expired());
-	print_whatever("\nexpired: ", p.use_count());
-	print_whatever("\ncontent: ");
 
-	if (const auto sp{ p.lock() }; sp)
-		print_whatever(sp->value, '\n');
-	else
-		print_whatever("<nullptr>");
-}
 int main(int argc, char**argv)
 {
-	STD_ON;
-	weak_ptr<Foo> weak_foo;
-	weak_ptr_info(weak_foo);
+	shared_ptr<string> shared_name;
+	shared_ptr<size_t> shared_age;
+
 	{
-		auto shared_foo{ make_shared<Foo>(1337) };
-		weak_foo = shared_foo;
-		weak_ptr_info(weak_foo);
+		auto sperson{ make_shared<person>("John Doe", 30) };
+
+		shared_name = shared_ptr<string>{ sperson, &sperson->name };
+		shared_age = shared_ptr<size_t>{ sperson, &sperson->age };
 	}
+	// person object is stil alive
+	print_whatever("Name: ", *shared_name, "\nage: ", *shared_age, '\n');
 
-	weak_ptr_info(weak_foo);
 
 
 
-	
 }
