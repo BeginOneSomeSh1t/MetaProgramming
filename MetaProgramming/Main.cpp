15ea2a9f3bb23e8ef3ce578c22c5e1bc8a7b0353
BeginOneSomeSh1t <Meganite@mail.ru>
2023-03-19 19:14:43 +0400

    workers and consumers are working in a parallel mode

--- MetaProgramming/Main.cpp
+++ MetaProgramming/Main.cpp
@@ -1,11 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <iomanip>
-#include <map>
-#include <string>
-#include <algorithm>
-#include <iterator>
-#include <future>
+#include <sstream>
+#include <vector>
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+
 
 template<typename...Args>
 void print_whatever(Args...args)
@@ -14,47 +17,79 @@ void print_whatever(Args...args)
 }
 
 using namespace std;
+using namespace chrono_literals;
 
-static map<char, size_t> histogram(const string& s)
+struct pcout : stringstream
 {
-	map<char, size_t> m;
-	for (char c : s)
-		m[c] += 1;
-	return m;
+	static inline mutex cout_mutex;
+	~pcout()
+	{
+		lock_guard<mutex> l{ cout_mutex };
+		cout << rdbuf();
+	}
+};
+
+queue<size_t> q;
+mutex q_mutex;
+bool production_stopped{ false };
+
+condition_variable go_produce;
+condition_variable go_consume;
+
+static void producer(size_t id, size_t items, size_t stock)
+{
+	for (size_t i{ 0 }; i < items; ++i)
+	{
+		unique_lock<mutex> l{ q_mutex };
+		go_produce.wait(l,
+			[&] {return q.size() < stock; });
+		q.push(id * 100 + i);
+		pcout{} << "   Producer " << id << " --> item "
+			<< setw(3) << q.back() << '\n';
+
+		go_consume.notify_all();
+		this_thread::sleep_for(90ms);
+	}
+	pcout{} << "EXIT: Producre " << id << '\n';
 }
 
-static string sorted(string s)
+static void consumer(size_t id)
 {
-	sort(begin(s), end(s));
-	return s;
+	while (!production_stopped || !q.empty())
+	{
+		unique_lock<mutex> l {q_mutex};
+		if (go_consume.wait_for(l, 1s,
+			[] {return !q.empty(); }))
+		{
+			pcout{} << "        item "
+				<< setw(3) << q.front()
+				<< " --> Consumer "
+				<< id << "\n";
+			q.pop();
+			go_produce.notify_all();
+			this_thread::sleep_for(130ms);
+		}
+	}
+	pcout{} << "EXIT: Consumer " << id << '\n';
 }
 
-static bool is_vowel(char c)
-{
-	char vowels[]{ "aeiou" };
-	return end(vowels) != find(begin(vowels), end(vowels), c);
-}
 
-static size_t vowels(const string& s)
-{
-	return count_if(begin(s), end(s), is_vowel);
-}
 
 int main(int argc, char**argv)
 {
-	cin.unsetf(ios::skipws);
-	string input{ istream_iterator<char>{cin}, {} };
-	input.pop_back();
+	vector<thread> workers;
+	vector<thread> consumers;
 
-	auto hist{ async(launch::async, histogram, input) };
-	auto sorted_str{ async(launch::async, sorted, input) };
-	auto vowel_count{ async(launch::async, vowels, input) };
+	for (size_t i{ 0 }; i < 3; ++i)
+		workers.emplace_back(producer, i, 15, 5);
+	for (size_t i{ 0 }; i < 5; ++i)
+		consumers.emplace_back(consumer, i);
 
-	for (const auto& [c, count] : hist.get())
-		cout << c << " : " << count << '\n';
-	cout << "sorted string: "
-		<< quoted(sorted_str.get()) << '\n'
-		<< "Total vowels: "
-		<< vowel_count.get() << '\n';
+	for (auto& t : workers)
+		t.join();
+	production_stopped = true;
+
+	for (auto& t : consumers)
+		t.join();
 
 }
