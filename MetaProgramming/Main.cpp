30d97a1ed218f086a76cbc87680486ff6ecab5e2
BeginOneSomeSh1t <Meganite@mail.ru>
2023-03-20 08:28:00 +0400

    UNIX grep in its all beauty!

--- MetaProgramming/Main.cpp
+++ MetaProgramming/Main.cpp
@@ -1,10 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <sstream>
-#include <iomanip>
-#include <numeric>
-#include <algorithm>
+#include <fstream>
+#include <regex>
 #include <vector>
+#include <string>
 #include <filesystem>
 
 
@@ -17,76 +16,50 @@ void print_whatever(Args...args)
 using namespace std;
 using namespace filesystem;
 
-using file_info = tuple<path, file_status, size_t>;
-
-
-static file_info
-get_file_info(const directory_entry& entry)
+static vector<pair<size_t, string>>
+matches(const path& p, const regex& re)
 {
-	const auto fs{ status(entry) };
-	return { entry.path(), fs, is_regular_file(fs) ? file_size(entry.path()) : 0u };
+	vector<pair<size_t, string>> d;
+	// possible mistake
+	ifstream is{ p.c_str() };
+	string s;
+	for (size_t line{ 1 }; getline(is, s); ++line)
+	{
+		if (regex_search(begin(s), end(s), re))
+			d.emplace_back(line, s);
+	}
+	return d;
 }
 
-
-static char type_char(file_status fs)
-{
-	if (is_directory(fs)) return 'd';
-	else if (is_symlink(fs)) return 'l';
-	else if (is_character_file(fs)) return 'c';
-	else if (is_block_file(fs)) return 'b';
-	else if (is_fifo(fs)) return 'p';
-	else if (is_socket(fs)) return 's';
-	else if (is_other(fs)) return 'o';
-	else if (is_regular_file(fs)) return 'f';
-	return '?';
-}
-
-static string rwx(perms p)
-{
-	auto check{ [p](perms bit, char c) {
-		return (p & bit) == perms::none ? '-' : c;
-	} };
-	return { check(perms::owner_read, 'r'),
-	 check(perms::owner_write, 'w'),
-	 check(perms::owner_exec, 'x'),
-	 check(perms::group_read, 'r'),
-	 check(perms::group_write, 'w'),
-	 check(perms::group_exec, 'x'),
-	 check(perms::others_read, 'r'),
-	 check(perms::others_write, 'w'),
-	 check(perms::others_exec, 'x') };
-}
-
-static string size_string(size_t size)
-{
-	stringstream ss;
-	if (size >= 1000000000) ss << (size / 1000000000) << 'G';
-	else if (size >= 1000000) ss << (size / 1000000) << 'M';
-	else if (size >= 1000) ss << (size / 1000) << 'K';
-	else ss << size << 'B';
-	return ss.str();
-}
 
 int main(int argc, char**argv)
 {
-	string path_s{ istream_iterator<char>{cin}, {} };
+	string regex_s{ istream_iterator<char>{cin}, { } };
 
-	path dir{ !path_s.empty() ? path_s : "." };
-	if (!exists(dir))
+	if (regex_s.empty())
 	{
-		cout << "Path " << dir << " doesn't exist.\n";
+		cout << "Empty. Enter someting\n";
 		return 1;
 	}
 
-	vector<file_info> items;
-	transform(directory_iterator{ dir }, {}, back_inserter(items), get_file_info);
-	
-	for (const auto& [path, status, size] : items)
+	regex pattern;
+	try
 	{
-		cout << type_char(status)
-			<< rwx(status.permissions()) << " "
-			<< setw(4) << right << size_string(size)
-			<< " " << path.filename().string()
-			<< '\n';
+		pattern = regex{ regex_s };
+	}
+	catch (const regex_error er)
+	{
+		cout << "Invalid regulare expression provided.\n";
+		return 1;
+	}
+
+	for (const auto& entry : recursive_directory_iterator{ current_path() })
+	{
+		auto ms{ matches(entry.path(), pattern) };
+		for (const auto& [number, content] : ms)
+		{
+			cout << entry.path().string() << " : " << number
+				<< " - " << content << '\n';
+		}
 	}
 }
