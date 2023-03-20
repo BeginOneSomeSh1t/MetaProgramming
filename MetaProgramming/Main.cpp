1ffcd7f9d8e2296703dbc373d4daedac726d02b3
BeginOneSomeSh1t <Meganite@mail.ru>
2023-03-20 10:37:11 +0400

    Replaces dupes within symlinks

--- MetaProgramming/Main.cpp
+++ MetaProgramming/Main.cpp
@@ -1,8 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <sstream>
-#include <iomanip>
-#include <map>
+#include <fstream>
+#include <unordered_map>
 #include <filesystem>
 #include "string_plus.h"
 
@@ -16,48 +15,70 @@ void print_whatever(Args...args)
 using namespace std;
 using namespace filesystem;
 
-using ext_info = map<string, pair<size_t, size_t>>;
-
-static ext_info ext_stats(const path& dir)
+static size_t hash_from_path(const path& p)
 {
-	ext_info m;
+	ifstream is{ p.c_str(), ios::in || ios::binary };
+	if (!is)
+		throw errno;
+	string s;
+	is.seekg(0, ios::end);
+	s.reserve(is.tellg());
+	is.seekg(0, ios::beg);
+	s.assign(istreambuf_iterator<char>{is}, {});
+	return hash<string>{}(s);
+}
+
+static size_t reduce_dupes(const path& dir)
+{
+	unordered_map<size_t, path> m;
+	size_t count{ 0 };
 	for (const auto& entry :
 		recursive_directory_iterator{ dir })
 	{
 		const path p{ entry.path() };
-		const file_status fs{ status(p) };
-		if (is_directory(fs)) { continue; }
-		const string ext{ p.extension().string() };
-		if (ext.length() == 0)
+		if (is_directory(p))
 			continue;
-
-		const size_t size{ file_size(p) };
-		auto& [size_accum, count] = m[ext];
-		size_accum += size;
-		count += 1;
+		const auto& [it, success] =
+			m.try_emplace(hash_from_path(p), p);
+		if (!success) {
+			cout << "Removed " << p.string()
+				<< " because it is a duplicate of "
+				<< it->second.string() << '\n';
+			remove(p);
+			create_symlink(absolute(it->second), p);
+			++count;
+		}
 	}
-	return m;
+	return count;
 }
 
 
 
 int main(int argc, char**argv)
 {
+	setlocale(LC_ALL, "rus");
 	args_string args{};
-	path dir{ args.argc > 1 ? args[1] : "." };
-	if (!exists(dir))
+
+	if (args.argc != 1)
 	{
-		cout << "Path " << dir << " does not exist";
+		cout << "Usage: " << args[0] << " <path>\n";
 		return 1;
 	}
-
-	for (const auto& [ext, stats] : ext_stats(dir))
+	path dir{ args[0] };
+	if (!exists(dir))
 	{
-		const auto& [accum_size, count] = stats;
-		cout << setw(15) << left << ext << ": "
-			<< setw(4) << right << count
-			<< " items, avg size "
-			<< setw(4) << size_string(accum_size / count)
-			<< '\n';
+		cout << "Path " << dir << " doesn't exist\n";
+		return 1;
 	}
+	try
+	{
+		const size_t dupes{ reduce_dupes(dir) };
+		cout << "Removed " << dupes << " diplicates.\n";
+	}
+	catch (filesystem_error er)
+	{
+		cout << "Er: " << er.what() << '\n';
+		return 1;
+	}
+	
 }
