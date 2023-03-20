6c23a60af4c54429521ec47769432cc4ffae0830
BeginOneSomeSh1t <Meganite@mail.ru>
2023-03-20 08:11:33 +0400

    enter a path to a directory and see what will happen

--- MetaProgramming/Main.cpp
+++ MetaProgramming/Main.cpp
@@ -1,10 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <sstream>
 #include <iomanip>
-#include <thread>
-#include <string>
-#include "stream_plus.h"
-#include <future>
+#include <numeric>
+#include <algorithm>
+#include <vector>
+#include <filesystem>
+
 
 template<typename...Args>
 void print_whatever(Args...args)
@@ -13,82 +15,78 @@ void print_whatever(Args...args)
 }
 
 using namespace std;
-using namespace chrono_literals;
+using namespace filesystem;
 
-static string create(const char* s)
+using file_info = tuple<path, file_status, size_t>;
+
+
+static file_info
+get_file_info(const directory_entry& entry)
 {
-	pcout{} << "3s CREATE " << quoted(s) << '\n';
-	this_thread::sleep_for(3s);
-	return { s };
+	const auto fs{ status(entry) };
+	return { entry.path(), fs, is_regular_file(fs) ? file_size(entry.path()) : 0u };
 }
 
-static string concat(const string& a, const string& b)
-{
-	pcout{} << "5s CONCAT"
-		<< quoted(a) << " "
-		<< quoted(b) << "\n";
-	this_thread::sleep_for(5s);
-	return a + b;
 
+static char type_char(file_status fs)
+{
+	if (is_directory(fs)) return 'd';
+	else if (is_symlink(fs)) return 'l';
+	else if (is_character_file(fs)) return 'c';
+	else if (is_block_file(fs)) return 'b';
+	else if (is_fifo(fs)) return 'p';
+	else if (is_socket(fs)) return 's';
+	else if (is_other(fs)) return 'o';
+	else if (is_regular_file(fs)) return 'f';
+	return '?';
 }
 
-static string twice(const string& s)
+static string rwx(perms p)
 {
-	pcout{} << "3s TWICE " << quoted(s) << '\n';
-	this_thread::sleep_for(3s);
-	return s + s;
+	auto check{ [p](perms bit, char c) {
+		return (p & bit) == perms::none ? '-' : c;
+	} };
+	return { check(perms::owner_read, 'r'),
+	 check(perms::owner_write, 'w'),
+	 check(perms::owner_exec, 'x'),
+	 check(perms::group_read, 'r'),
+	 check(perms::group_write, 'w'),
+	 check(perms::group_exec, 'x'),
+	 check(perms::others_read, 'r'),
+	 check(perms::others_write, 'w'),
+	 check(perms::others_exec, 'x') };
 }
 
-template<typename _F>
-static auto asynchronize(_F f)
+static string size_string(size_t size)
 {
-	return [f](auto...xs)
-	{
-		return [=]()
-		{
-			return async(launch::async, f, xs...);
-		};
-	};
+	stringstream ss;
+	if (size >= 1000000000) ss << (size / 1000000000) << 'G';
+	else if (size >= 1000000) ss << (size / 1000000) << 'M';
+	else if (size >= 1000) ss << (size / 1000) << 'K';
+	else ss << size << 'B';
+	return ss.str();
 }
-
-template<typename _F>
-static auto fut_unwrap(_F f)
-{
-	return [f](auto...xs)
-	{
-		return f(xs.get()...);
-	};
-}
-
-template<typename _F>
-static auto async_adapter(_F f)
-{
-	return [f](auto...xs)
-	{
-		return [=]()
-		{
-			return async(launch::async, fut_unwrap(f), xs()...);
-		};
-	};
-}
-
 
 int main(int argc, char**argv)
 {
-	auto pcreate{ asynchronize(create) };
-	auto pconcat{ async_adapter(concat) };
-	auto ptwice{ async_adapter(twice) };
-	
-	auto result(
-		pconcat(
-			ptwice(
-				pconcat(
-					pcreate("foo "),
-					pcreate("bar "))),
-			pconcat(
-				pcreate("this "),
-				pcreate("that "))));
+	string path_s{ istream_iterator<char>{cin}, {} };
 
-	cout << "Setup done. Nothing executed yet.\n";
-	cout << result().get() << '\n';
+	path dir{ !path_s.empty() ? path_s : "." };
+	if (!exists(dir))
+	{
+		cout << "Path " << dir << " doesn't exist.\n";
+		return 1;
+	}
+
+	vector<file_info> items;
+	transform(directory_iterator{ dir }, {}, back_inserter(items), get_file_info);
+	
+	for (const auto& [path, status, size] : items)
+	{
+		cout << type_char(status)
+			<< rwx(status.permissions()) << " "
+			<< setw(4) << right << size_string(size)
+			<< " " << path.filename().string()
+			<< '\n';
+	}
 }
